06262baab7604b125b6e8d540535b1e1e58db311
Ryan <ryannolan1990@hotmail.com>
2024-02-08 09:33:13 +0000

    this will make the questions nav bar only highlight the selected topic on click

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,15 +1,27 @@
 const burgerNav = document.getElementById("burger-nav");
 const menuOverlay = document.querySelector(".mobile-menu-overlay");
 
-
 burgerNav.addEventListener("click", toggleMenu);
 
-
 function toggleMenu() {
-    menuOverlay.classList.toggle("nav-active");
-    if (menuOverlay.classList[1] === "nav-active") {
-        burgerNav.src="./assets/shared/mobile/icon-close.svg";
-    } else {
-        burgerNav.src="./assets/shared/mobile/icon-hamburger.svg";
-    }
+  menuOverlay.classList.toggle("nav-active");
+  if (menuOverlay.classList[1] === "nav-active") {
+    burgerNav.src = "./assets/shared/mobile/icon-close.svg";
+  } else {
+    burgerNav.src = "./assets/shared/mobile/icon-hamburger.svg";
+  }
 }
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  question.addEventListener("click", increaseOpacity);
+
+  function increaseOpacity() {
+    questions.forEach((question) => {
+      question.style.opacity = "40%";
+    });
+
+    question.style.opacity = "100%";
+  }
+});
