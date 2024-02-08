288d9c1fa8869b1fa26fc43689149f6a41059e2b
Ryan <ryannolan1990@hotmail.com>
2024-02-08 11:11:26 +0000

    this will add the functionality to display the answers to the questions when the green arrows are clicked

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,6 +1,8 @@
 const burgerNav = document.getElementById("burger-nav");
 const menuOverlay = document.querySelector(".mobile-menu-overlay");
 const questions = document.querySelectorAll(".question");
+const greenArrows = document.querySelectorAll(".green-arrow");
+const options = document.querySelectorAll(".options");
 
 burgerNav.addEventListener("click", toggleMenu);
 
@@ -13,16 +15,28 @@ function toggleMenu() {
   }
 }
 
-
 questions.forEach((question) => {
   question.addEventListener("click", increaseOpacity);
 
   function increaseOpacity() {
     questions.forEach((question) => {
       question.style.opacity = "40%";
-      question.querySelector(".question-number").style.color = "rgb(51, 61, 75)";
+      question.querySelector(".question-number").style.color =
+        "rgb(51, 61, 75)";
     });
     question.style.opacity = "100%";
     question.querySelector(".question-number").style.color = "#0E8784";
+  }
+});
+
+greenArrows.forEach((arrow, index) => {
+  arrow.addEventListener("click", function () {
+    toggleOptions(index);
+  });
+
+  function toggleOptions(index) {
+    options[index].style.height = "100%";
+    options[index].style.visibility = "visible";
+    options[index].style.opacity = "100%";
   }
 });
